6b7190f0d66003248d09ebc57623c95994d6bb5f
gitnew03 <gitpractice03>
2023-09-11 11:24:21 +0530

    add dec.py

---
+++ dec.py
@@ -0,0 +1,21 @@
+pipeline {
+  agent any {
+    stages {
+      stage('one') {
+        steps {
+          echo 'Hi, this is Ayesha.'
+        }
+      }
+      stage('two') {
+        steps {
+          input('Do you want to proceed')
+        }
+      }
+      stage('three') {
+        steps {
+          echo 'Done!'
+        }
+      }
+    }
+  }
+}
